68630bbc6853ef03726ad905f1fdeeb02a985498
d2kx <dennis@dmh.dev>
2024-05-20 03:51:30 +0200

    add basic components

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CallToAction from './CallToAction';
+import LocationInput from './LocationInput';
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          dmh-ipapi-frontend
+        </Typography>
+        <LocationInput />
+        <CallToAction />
+      </Box>
+    </Container>
+  );
+}
---
+++ src/CallToAction.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function CallToAction() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'please hire me <3'}
+    </Typography>
+  );
+}
