7002bafe05321a9c92b5319249c938afa7156f98
d2kx <dennis@dmh.dev>
2024-05-20 05:38:39 +0200

    get stuff working with basic input, finish it tmrw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import CallToAction from './CallToAction';
 import LocationInput from './LocationInput';
+import { useDebounce } from 'use-debounce';
 
 export default function App() {
+  const [ipAddressValue, setIpAddressValue] = useState<string>('');
+  const [locationResult, setLocationResult] = useState('');
+
+  const [debounceInput] = useDebounce(ipAddressValue, 1000);
+
+  const URL = 'http://localhost:3100/api/location/';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (debounceInput.trim() === '') {
+        setLocationResult('');
+        return;
+      }
+      try {
+        const response = await fetch(URL + debounceInput);
+        const data = await response.json();
+        setLocationResult(data);
+      } catch (error) {}
+    };
+
+    fetchData();
+  }, [debounceInput]);
+
   return (
+    <div>
+      <input
+        value={ipAddressValue}
+        type="search"
+        onChange={(e) => setIpAddressValue(e.target.value)}
+      />
+      <div>{locationResult ? JSON.stringify(locationResult) : ''}</div>
+    </div>
+  );
+
+  /* return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
@@ -16,5 +51,5 @@ export default function App() {
         <CallToAction />
       </Box>
     </Container>
-  );
+  ;*/
 }
--- src/LocationInput.tsx
+++ src/LocationInput.tsx
@@ -1,8 +1,42 @@
-import * as React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import CircularProgress from '@mui/material/CircularProgress';
+import FormControl, { useFormControl } from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import Typography from '@mui/material/Typography';
 
-export default function BasicTextFields() {
+function sleep(duration: number): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+}
+
+function CountryText() {
+  const { filled } = useFormControl() || {};
+
+  const country = useMemo(() => {
+    if (filled) {
+      async () => {
+        await sleep(1e3);
+        console.log('test');
+      };
+
+      return 'DE';
+    }
+
+    return 'USA';
+  }, [filled]);
+
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {country}
+    </Typography>
+  );
+}
+
+export default function LocationInput() {
   return (
     <Box
       component="form"
@@ -12,7 +46,10 @@ export default function BasicTextFields() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <FormControl>
+        <Input placeholder="hi" />
+        <CountryText />
+      </FormControl>
     </Box>
   );
 }
