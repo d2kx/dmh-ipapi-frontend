8f8e6a04dc5f4e4f546fc4991b118f58b337095f
d2kx <dennis@dmh.dev>
2024-05-21 04:14:36 +0200

    merge loading boolean into LocationState

--- src/LocationInput.tsx
+++ src/LocationInput.tsx
@@ -5,15 +5,14 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Input from '@mui/material/Input';
 import Typography from '@mui/material/Typography';
 import { useDebounce } from 'use-debounce';
-import PendingIcon from '@mui/icons-material/Pending';
 import CheckIcon from '@mui/icons-material/Check';
 import ErrorIcon from '@mui/icons-material/Error';
-import { green } from '@mui/material/colors';
 
-interface Location {
+interface LocationState {
   city?: string;
   country?: string;
   ipAddress?: string;
+  loading: boolean;
   message?: string;
   status: 'error' | 'fail' | 'ready' | 'success';
 }
@@ -21,14 +20,19 @@ interface Location {
 const URL = 'http://localhost:3100/api/location/';
 
 export default function LocationInput() {
-  const [loading, setLoading] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>('');
-  const [location, setLocation] = useState<Location>({ status: 'ready' });
+  const [locationState, setLocationState] = useState<LocationState>({
+    loading: false,
+    status: 'ready',
+  });
 
   const [debounceInput] = useDebounce(inputValue, 1000);
 
   const handleInput = (input: string) => {
-    setLoading(input === debounceInput ? false : true);
+    setLocationState({
+      ...locationState,
+      loading: input === debounceInput ? false : true,
+    });
 
     setInputValue(input);
   };
@@ -36,19 +40,17 @@ export default function LocationInput() {
   useEffect(() => {
     const fetchData = async () => {
       if (!debounceInput) {
-        setLoading(false);
-        setLocation({ status: 'ready' });
+        setLocationState({ loading: false, status: 'ready' });
         return;
       }
 
       try {
         const response = await fetch(URL + debounceInput);
         const data = await response.json();
-        setLoading(false);
-        setLocation(data);
+        setLocationState({ ...data, loading: false });
       } catch (error) {
-        setLoading(false);
-        setLocation({
+        setLocationState({
+          loading: false,
           message: 'could not reach dmh-ipapi-backend',
           status: 'error',
         });
@@ -67,21 +69,21 @@ export default function LocationInput() {
           type="search"
         />
         <Stack direction="row" spacing={2}>
-          {loading ? (
+          {locationState.loading ? (
             <CircularProgress color="info" />
           ) : (
             <div>
-              {location.status === 'success' && (
+              {locationState.status === 'success' && (
                 <div>
                   <CheckIcon color="success" sx={{ fontSize: 40 }} />
                   <ul>
-                    <li>Stadt: {location.city}</li>
-                    <li>Country: {location.country}</li>
-                    <li>IP address: {location.ipAddress}</li>
+                    <li>Stadt: {locationState.city}</li>
+                    <li>Country: {locationState.country}</li>
+                    <li>IP address: {locationState.ipAddress}</li>
                   </ul>
                 </div>
               )}
-              {location.status === 'fail' && (
+              {locationState.status === 'fail' && (
                 <div>
                   <ErrorIcon color="error" sx={{ fontSize: 40 }} />
                   <ul>
@@ -89,11 +91,11 @@ export default function LocationInput() {
                       Oh no! The external IP Geolocation API has not been able
                       to process your request
                     </li>
-                    <li>Error: {location.message}</li>
+                    <li>Error: {locationState.message}</li>
                   </ul>
                 </div>
               )}
-              {location.status === 'error' && (
+              {locationState.status === 'error' && (
                 <div>
                   <ErrorIcon color="error" sx={{ fontSize: 40 }} />
                   <ul>
@@ -101,7 +103,7 @@ export default function LocationInput() {
                       Oh no! The dmh-ipapi-backend has not been able to process
                       your request
                     </li>
-                    <li>Error: {location.message}</li>
+                    <li>Error: {locationState.message}</li>
                   </ul>
                 </div>
               )}
