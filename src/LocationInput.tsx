fcccfd71e654115bf31d090adcdfd19f682e3af8
d2kx <dennis@dmh.dev>
2024-05-21 02:58:20 +0200

    improved error handling

--- src/LocationInput.tsx
+++ src/LocationInput.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import CircularProgress from '@mui/material/CircularProgress';
 import Input from '@mui/material/Input';
 import Typography from '@mui/material/Typography';
 import { useDebounce } from 'use-debounce';
-import { Container } from '@mui/material';
 
 interface Location {
   city?: string;
   country?: string;
   ipAddress?: string;
-  status: string;
+  message?: string;
+  status: 'error' | 'fail' | 'ready' | 'success';
 }
 
 const URL = 'http://localhost:3100/api/location/';
@@ -44,7 +44,10 @@ export default function LocationInput() {
         setLocation(data);
       } catch (error) {
         setLoading(false);
-        setLocation({ status: 'error' });
+        setLocation({
+          message: 'could not reach dmh-ipapi-backend',
+          status: 'error',
+        });
       }
     };
 
@@ -55,14 +58,41 @@ export default function LocationInput() {
     <Box sx={{ width: '100%' }}>
       <Stack spacing={2}>
         <Input
-          placeholder="IPv4/IPv6"
-          value={inputValue}
           onChange={(e) => handleInput(e.target.value)}
+          placeholder="valid IPv4/IPv6 address"
+          type="search"
         />
         {loading ? (
           <CircularProgress />
         ) : (
-          <div>{location ? JSON.stringify(location) : ''}</div>
+          <div>
+            <div>{location ? JSON.stringify(location) : ''}</div>
+            {location.status === 'success' && (
+              <ul>
+                <li>Stadt: {location.city}</li>
+                <li>Country: {location.country}</li>
+                <li>IP address: {location.ipAddress}</li>
+              </ul>
+            )}
+            {location.status === 'fail' && (
+              <ul>
+                <li>
+                  Oh no! The external IP Geolocation API has not been able to
+                  process your request
+                </li>
+                <li>Error: {location.message}</li>
+              </ul>
+            )}
+            {location.status === 'error' && (
+              <ul>
+                <li>
+                  Oh no! The dmh-ipapi-backend has not been able to process your
+                  request
+                </li>
+                <li>Error: {location.message}</li>
+              </ul>
+            )}
+          </div>
         )}
       </Stack>
     </Box>
